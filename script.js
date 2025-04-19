28134eb3b971c6ede15c73e499a0b17ca95e77be
PuebloCHC <DefenderOTC@gmail.com>
2025-04-19 17:20:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -94,3 +94,26 @@ function saveAnnouncementsToStorage() {
     saveAnnouncementsToStorage();
   }
   
+  <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDoP12wQz5Rw9RamCq-v7swBZCL5zC602M",
+    authDomain: "pchc-web-changes.firebaseapp.com",
+    projectId: "pchc-web-changes",
+    storageBucket: "pchc-web-changes.firebasestorage.app",
+    messagingSenderId: "982218349771",
+    appId: "1:982218349771:web:7cdc052b0e4bddb6524f4e",
+    measurementId: "G-3BBDS55VMW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
